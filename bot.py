ee206356af8f02a2491bd318c29c16eae7667287
zakyldrk <98780961+zakyldrk@users.noreply.github.com>
2022-02-07 17:26:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 BOT BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/banlgidongtele"
         )
         self.username = usr_bot_me.username
 
